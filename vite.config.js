d8da09db57610fba76c03b78a8dd431d844a40c5
B.pranav <bonganep@interpay.sa>
2025-03-15 11:06:55 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    outDir: 'build', // Ensure this is set to 'build' or the desired output directory
-  },
+  base: '/shrinivas-portfolio/',  // Adjust this to the name of your repo
 });
